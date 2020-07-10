6804c69bf147125b94d0a3f4c15d5a1e04107b15
BoYanZh <32470225+BoYanZh@users.noreply.github.com>
2020-07-10 10:17:25 +0800

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='canvassyncer',
-    version='1.1.3',
+    version='1.1.4',
     description='A canvas file syner',
     url='https://github.com/BoYanZh/Canvas-Syncer',
     author='SJTU JI Tech',
