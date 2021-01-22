8f0b348bb772a4d2dd424d692274392ec6b59292
Muchen Xu <flandre@umich.edu>
2021-01-22 22:48:40 +0800

    fixed '/' being removed from folder name

--- canvassyncer/__main__.py
+++ canvassyncer/__main__.py
@@ -233,7 +233,7 @@ class CanvasSyncer:
                     "course files", "")
                 if not res[folder['id']]:
                     res[folder['id']] = '/'
-                res[folder['id']] = re.sub(r"[\/\\\:\*\?\"\<\>\|]", "_",
+                res[folder['id']] = re.sub(r"[\\\:\*\?\"\<\>\|]", "_",
                                            res[folder['id']])
             page += 1
         return res
