bd4692d22bde1edde75a52d2704eda20b3645654
BoYanZh <boyanzh233@gmail.com>
2022-08-31 14:50:39 +0800

    chore: bump version to 2.0.6

--- canvassyncer/__main__.py
+++ canvassyncer/__main__.py
@@ -12,7 +12,7 @@ import aiofiles
 import httpx
 from tqdm import tqdm
 
-__version__ = "2.0.5"
+__version__ = "2.0.6"
 CONFIG_PATH = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), ".canvassyncer.json"
 )
